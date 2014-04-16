b7184bd18ef920906675745018eb8d84577d6ac8
Dario Giovannetti <dev@dariogiovannetti.net>
2014-04-16 15:27:37 +0800

    Str: improve code style

--- src/Str.js
+++ src/Str.js
@@ -1,19 +1,19 @@
 /*
  *  JavaScript auxiliary library
  *  Copyright (C) 2012-2013 Dario Giovannetti <dev@dariogiovannetti.net>
- * 
+ *
  *  This file is part of JavaScript auxiliary library.
- * 
+ *
  *  JavaScript auxiliary library is free software: you can redistribute it
  *  and/or modify it under the terms of the GNU General Public License
  *  as published by the Free Software Foundation, either version 3
  *  of the License, or (at your option) any later version.
- * 
+ *
  *  JavaScript auxiliary library is distributed in the hope that it will be
  *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with JavaScript auxiliary library.
  *  If not, see <http://www.gnu.org/licenses/>.
@@ -26,17 +26,18 @@ Alib.Str = new function () {
         if (!id) id = 0;
         return string.substring(0, id) + newString + string.substr(id);
     };
-    
+
     this.overwriteFor = function (string, newString, id, length) {
         if (!id) id = 0;
         if (!length || length < 0) length = 0;
-        return string.substring(0, id) + newString + string.substr(id + length);
+        return string.substring(0, id) + newString + string.substr(id +
+                                                                    length);
     };
-    
+
     this.overwriteAt = function (string, newString, id) {
         return this.overwriteFor(string, newString, id, newString.length);
     };
-    
+
     this.overwriteBetween = function (string, newString, id1, id2) {
         if (!id1) id1 = 0;
         if (!id2) id2 = id1;
@@ -47,22 +48,22 @@ Alib.Str = new function () {
         }
         return string.substring(0, id1) + newString + string.substr(id2);
     };
-    
+
     this.removeFor = function (string, id, length) {
         return this.overwriteFor(string, "", id, length);
     };
-    
+
     this.removeBetween = function (string, id1, id2) {
         return this.overwriteBetween(string, "", id1, id2);
     };
-    
+
     this.padLeft = function (string, filler, length) {
         while (string.length < length) {
             string = filler + string;
         }
         return string;
     };
-    
+
     this.padRight = function (string, filler, length) {
         while (string.length < length) {
             string += filler;
