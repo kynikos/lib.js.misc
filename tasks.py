2a2038f22f766345d7d61a55cfad7801343c662a
Dario Giovannetti <dev@dariogiovannetti.net>
2018-01-21 20:45:39 +0800

    Publish on npm

--- tasks.py
+++
@@ -1,9 +0,0 @@
-from invoke import task, run
-
-
-@task
-def compile(ctx):
-    """
-    Compile the CoffeeScript files to JavaScript.
-    """
-    run("coffee --compile --bare --output ./dist/ ./src/*.coffee")
